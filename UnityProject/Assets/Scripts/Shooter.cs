9fa85110418b0f669db59f69481c3e3ec8dca095
Phillip Patti <p_patti7@yahoo.com>
2013-04-23 20:56:15 -0500

    Book Project
    Completely changed repository to the book's work up to about Chapter 7.
    Open the project and then the Island scene for a pretty applicable test
    world.

---
+++ UnityProject/Assets/Scripts/Shooter.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+
+public class Shooter : MonoBehaviour {
+	public Rigidbody bullet;
+	public float power = 6000f;
+	public float moveSpeed = 4f;
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+		float v = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+		
+		transform.Translate(h, v, 0);
+		
+		if(Input.GetButtonUp("Fire1")){
+			Rigidbody instance = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+			Vector3 fwd = transform.TransformDirection(Vector3.forward);
+			instance.AddForce(fwd * power);
+		}
+	}
+}
