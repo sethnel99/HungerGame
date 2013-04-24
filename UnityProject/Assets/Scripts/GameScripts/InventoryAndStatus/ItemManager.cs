e6658f369673f1f6a4db1ea6343f16d5371c3744
Seth Nelson <sethnelson2013@u.northwestern.edu>
2013-04-24 01:04:24 -0500

    @Matt - Merged your Gatherable.cs code into my ItemManager class (which interacts with the Item class and Inventory class). Inventory is a MonoBehavior that gets attached to the player. ItemManager is a MonoBehavior which gets attached to the item (in place of your Gatherable class).
    When your code triggers (when the button has been held long enough) this code finds the Inventory Component on the player and calls the AddItem() method.
    
    I left your Gatherable.cs in case you want to tweak more things, but feel free to edit the ItemManager.cs file directly.

--- UnityProject/Assets/Scripts/GameScripts/InventoryAndStatus/ItemManager.cs
+++ UnityProject/Assets/Scripts/GameScripts/InventoryAndStatus/ItemManager.cs
@@ -4,21 +4,42 @@ using System.Collections;
 public class ItemManager : MonoBehaviour {
 	Item item;
 	
+	bool inZone;
+	float buttonDownTime;
+	public float gatherTime = 2.0f;
+	
 	// Use this for initialization
 	void Start () {
+		inZone = false;
+		buttonDownTime = 0.0f;
+		
 		item = new Item("TestItem",15);
 		item.addItemType(Item.material);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (inZone && Input.GetButton ("Fire1")) {
+			buttonDownTime += Time.deltaTime;
+			if (buttonDownTime > gatherTime) {
+				GameObject player  = GameObject.FindWithTag("Player");
+				player.GetComponent<Inventory>().addItem (this.item);
+				Destroy (gameObject);
+			}	
+		}
+		else {buttonDownTime = 0.0f;}
 	}
 	
-	void OnTriggerEnter(Collider col){
-		if(col.gameObject.tag == "Player"){
-			col.gameObject.GetComponent<Inventory>().addItem (this.item);
-			Destroy(gameObject);
+	
+	void OnTriggerEnter (Collider col) {
+		if (col.gameObject.tag == "Player") {
+			inZone = true;
+		}
+	}
+	
+	void OnTriggerExit (Collider col) {
+		if (col.gameObject.tag == "Player") {
+			inZone = false;
 		}
 	}
 }
