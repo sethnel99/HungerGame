d5997e5f6bb7e6c752930628a69a5a4d09a5311a
Phillip Patti <p_patti7@yahoo.com>
2013-05-14 19:04:34 -0500

    Dog (rigged, animated (probably needs modifications), imported)

---
+++ UnityProject/Assets/Models/Doberman/Dog.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class Dog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(COMainBehavior());
+	}
+	
+	IEnumerator COMainBehavior() {
+	    //yield return new WaitForSeconds(0.1f); // trigger delay
+	    // create bullet and flash effect...
+		gameObject.animation.Play("Stand_Idle");
+	    //yield return 0; // wait 1 frame
+	    // stop flash
+	    yield return new WaitForSeconds(2f);
+	    gameObject.animation.Play("Idle_Twitch_1");
+	    yield return new WaitForSeconds(3f); // extra delay before you can shoot again
+	    gameObject.animation.Play("Bark_1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
