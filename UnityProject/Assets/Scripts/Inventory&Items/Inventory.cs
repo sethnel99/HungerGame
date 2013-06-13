1bd28ec2008e34a88217210736c5da58660e4b07
Seth Nelson <sethnelson2013@u.northwestern.edu>
2013-06-12 21:53:20 -0500

    fixed typo in inventory.cs

--- UnityProject/Assets/Scripts/Inventory&Items/Inventory.cs
+++ UnityProject/Assets/Scripts/Inventory&Items/Inventory.cs
@@ -51,7 +51,7 @@ public class Inventory : MonoBehaviour{
 
         craftingDictionary.Add(new SidePlankItem(2), new Item[] { new WoodItem(1) });
         craftingDictionary.Add(new RoofPlankItem(2), new Item[] { new WoodItem(1) });
-        craftingDictionary.Add(new DoorItem(1), new Item[] { new WoodItem(10), new StoneItem(4) });
+        craftingDictionary.Add(new DoorItem(1), new Item[] { new WoodItem(10), new SharpStoneItem(4) });
         craftingDictionary.Add(new FireItem(1), new Item[] { new WoodItem(3), new SharpStoneItem(2)});
         craftingDictionary.Add(new SmallCookedMeatItem(1), new Item[] { new SmallUncookedMeatItem(1) });
         craftingDictionary.Add(new LargeCookedMeatItem(1), new Item[] { new LargeUncookedMeatItem(1) });
